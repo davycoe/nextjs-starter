608ff522046f8f63b9881129e68532e1379971c5
dave coe <hello@davecoe.dev>
2022-03-25 03:25:45 +0000

    config: added prettier-plugin-tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-import type { NextPage } from "next";
-
-const Home: NextPage = () => {
-  return <h1 className="text-5xl font-bold underline">Hello world!</h1>;
+const Home = () => {
+  return (
+    <h1 className="flex justify-center pt-12 text-7xl font-bold underline">
+      Hello world!
+    </h1>
+  );
 };
 
 export default Home;
